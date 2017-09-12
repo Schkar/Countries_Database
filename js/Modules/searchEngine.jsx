b7c5ce753c8e8c87ee77db1d318ad93b4905312f
Karol Schweitzer <karol.schweitzer@gmail.com>
2017-09-12 13:19:28 +0200

    Searching need more work, but it works.

--- js/Modules/searchEngine.jsx
+++ js/Modules/searchEngine.jsx
@@ -6,23 +6,32 @@ class SearchEngineButton extends React.Component {
 
         this.state = {
             currentResponse: "",
-            searchQuery: "usa"
+            searchQuery: ""
         }
     }
 
     // shouldComponentUpdate(){
     //     if (this.props.clickedCountry !== "") {
-    //         this.searchStart()
+    //         this.searchStart(this.props.clickedCountry)
+    //         return true;
     //     }
+    //     return false;
     // }
 
-    searchStart = () =>{
+    searchStart = (event) =>{
         // if (this.state.searchQuery === "") {
         //     console.log("query is empty");
         //     //TODO: Display something somewhere, when props.query is empty.
         //     return;
         // }
-        fetch("https://restcountries.eu/rest/v2/name/"+this.state.searchQuery).then( (r) => {
+        let countryToSearch = this.props.currentSearchQuery;
+        console.log(countryToSearch, this.props.currentSearchQuery);
+        console.log(event);
+        // if (event !== "") {
+        //     countryToSearch = event;
+        // }
+        
+        fetch("https://restcountries.eu/rest/v2/name/"+countryToSearch).then( (r) => {
             //TODO: Change above state to props and remove searchQuery from state. Not needed
             if (r.ok) {
                 return r.json()
@@ -41,7 +50,7 @@ class SearchEngineButton extends React.Component {
 
     render(){
         return(
-            <button className="searchButton normalSearchButton" onClick={this.searchStart} value={this.props.text}>{this.props.text}</button>
+            <button className="searchButton normalSearchButton" onKeyPress={this.searchStart} onClick={this.searchStart} value={this.props.text}>{this.props.text}</button>
         )
     }
 }
@@ -53,7 +62,7 @@ class LuckySearchEngineButton extends React.Component {
         this.state = {
             countries: 
             [
-                "Andorra","United Arab Emirates","Afghanistan","Antigua and Barbuda","Anguilla","Albania","Armenia","Angola","Argentina","American Samoa","Austria","Australia","Aruba","Aland Islands","Azerbaijan","Bosnia and Herzegovina","Barbados","Bangladesh","Belgium","Burkina Faso","Bulgaria","Bahrain","Burundi","Benin","Saint Barthelemy","Brunei Darussalam","Bolivia","Bermuda","Bonaire","Brazil","Bahamas","Bhutan","Bouvet Island","Botswana","Belarus","Belize","Canada","Cocos (Keeling) Islands","Democratic Republic of Congo","Central African Republic","Republic of Congo","Switzerland","Côte d'Ivoire","Cook Islands","Chile","Cameroon","China","Colombia","Costa Rica","Cuba","Cape Verde","Curaçao","Christmas Island","Cyprus","Czech Republic","Germany","Djibouti","Denmark","Dominica","Dominican Republic","Algeria","Ecuador","Egypt","Estonia","Western Sahara","Eritrea","Spain","Ethiopia","Finland","Fiji","Falkland Islands","Federated States of Micronesia","Faroe Islands","France","Gabon","United Kingdom","Georgia","Grenada","French Guiana","Guernsey","Ghana","Gibraltar","Greenland","Gambia","Guinea","Glorioso Islands","Guadeloupe","Equatorial Guinea","Greece","South Georgia and South Sandwich Islands","Guatemala","Guam","Guinea-Bissau","Guyana","Hong Kong","Heard Island and McDonald Islands","Honduras","Croatia","Haiti","Hungary","Indonesia","Ireland","Israel","Isle of Man","India","British Indian Ocean Territory","Iraq","Iran","Iceland","Italy","Jersey","Jamaica","Jordan","Japan","Juan De Nova Island","Kenya","Kyrgyzstan","Cambodia","Kiribati","Comoros","Saint Kitts and Nevis","North Korea","South Korea","Kosovo","Kuwait","Cayman Islands","Kazakhstan","Lao People's Democratic Republic","Lebanon","Saint Lucia","Liechtenstein","Sri Lanka","Liberia","Lesotho","Lithuania","Luxembourg","Latvia","Libya","Morocco","Monaco","Moldova","Madagascar","Montenegro","Saint Martin","Marshall Islands","Macedonia","Mali","Macau","Myanmar","Mongolia","Northern Mariana Islands","Martinique","Mauritania","Montserrat","Malta","Mauritius","Maldives","Malawi","Mexico","Malaysia","Mozambique","Namibia","New Caledonia","Niger","Norfolk Island","Nigeria","Nicaragua","Netherlands","Norway","Nepal","Nauru","Niue","New Zealand","Oman","Panama","Peru","French Polynesia","Papua New Guinea","Philippines","Pakistan","Poland","Saint Pierre and Miquelon","Pitcairn Islands","Puerto Rico","Palestinian Territories","Portugal","Palau","Paraguay","Qatar","Reunion","Romania","Serbia","Russia","Rwanda","Saudi Arabia","Solomon Islands","Seychelles","Sudan","Sweden","Singapore","Saint Helena","Slovenia","Svalbard and Jan Mayen","Slovakia","Sierra Leone","San Marino","Senegal","Somalia","Suriname","South Sudan","Sao Tome and Principe","El Salvador","Sint Maarten","Syria","Swaziland","Turks and Caicos Islands","Chad","French Southern and Antarctic Lands","Togo","Thailand","Tajikistan","Tokelau","Timor-Leste","Turkmenistan","Tunisia","Tonga","Turkey","Trinidad and Tobago","Tuvalu","Taiwan","Tanzania","Ukraine","Uganda","Jarvis Island","Baker Island","Howland Island","Johnston Atoll","Midway Islands","Wake Island","United States","Uruguay","Uzbekistan","Vatican City","Saint Vincent and the Grenadines","Venezuela","British Virgin Islands","US Virgin Islands","Vietnam","Vanuatu","Wallis and Futuna","Samoa","Yemen","Mayotte","South Africa","Zambia","Zimbabwe"
+                "Andorra","United Arab Emirates","Afghanistan","Antigua and Barbuda","Anguilla","Albania","Armenia","Angola","Argentina","American Samoa","Austria","Australia","Aruba","Aland Islands","Azerbaijan","Bosnia and Herzegovina","Barbados","Bangladesh","Belgium","Burkina Faso","Bulgaria","Bahrain","Burundi","Benin","Saint Barthelemy","Brunei Darussalam","Bolivia","Bermuda","Bonaire","Brazil","Bahamas","Bhutan","Bouvet Island","Botswana","Belarus","Belize","Canada","Cocos (Keeling) Islands","Democratic Republic of Congo","Central African Republic","Republic of Congo","Switzerland","Côte d'Ivoire","Cook Islands","Chile","Cameroon","China","Colombia","Costa Rica","Cuba","Cape Verde","Curaçao","Christmas Island","Cyprus","Czech Republic","Germany","Djibouti","Denmark","Dominica","Dominican Republic","Algeria","Ecuador","Egypt","Estonia","Western Sahara","Eritrea","Spain","Ethiopia","Finland","Fiji","Falkland Islands","Federated States of Micronesia","Faroe Islands","France","Gabon","United Kingdom","Georgia","Grenada","French Guiana","Guernsey","Ghana","Gibraltar","Greenland","Gambia","Guinea","Glorioso Islands","Guadeloupe","Equatorial Guinea","Greece","South Georgia and South Sandwich Islands","Guatemala","Guam","Guinea-Bissau","Guyana","Hong Kong","Heard Island and McDonald Islands","Honduras","Croatia","Haiti","Hungary","Indonesia","Ireland","Israel","Isle of Man","India","British Indian Ocean Territory","Iraq","Iran","Iceland","Italy","Jersey","Jamaica","Jordan","Japan","Kenya","Kyrgyzstan","Cambodia","Kiribati","Comoros","Saint Kitts and Nevis","North Korea","South Korea","Kosovo","Kuwait","Cayman Islands","Kazakhstan","Lao People's Democratic Republic","Lebanon","Saint Lucia","Liechtenstein","Sri Lanka","Liberia","Lesotho","Lithuania","Luxembourg","Latvia","Libya","Morocco","Monaco","Moldova","Madagascar","Montenegro","Saint Martin","Marshall Islands","Macedonia","Mali","Macau","Myanmar","Mongolia","Northern Mariana Islands","Martinique","Mauritania","Montserrat","Malta","Mauritius","Maldives","Malawi","Mexico","Malaysia","Mozambique","Namibia","New Caledonia","Niger","Norfolk Island","Nigeria","Nicaragua","Netherlands","Norway","Nepal","Nauru","Niue","New Zealand","Oman","Panama","Peru","French Polynesia","Papua New Guinea","Philippines","Pakistan","Poland","Saint Pierre and Miquelon","Pitcairn Islands","Puerto Rico","Palestinian Territories","Portugal","Palau","Paraguay","Qatar","Reunion","Romania","Serbia","Russia","Rwanda","Saudi Arabia","Solomon Islands","Seychelles","Sudan","Sweden","Singapore","Saint Helena","Slovenia","Svalbard and Jan Mayen","Slovakia","Sierra Leone","San Marino","Senegal","Somalia","Suriname","South Sudan","Sao Tome and Principe","El Salvador","Sint Maarten","Syria","Swaziland","Turks and Caicos Islands","Chad","French Southern and Antarctic Lands","Togo","Thailand","Tajikistan","Tokelau","Timor-Leste","Turkmenistan","Tunisia","Tonga","Turkey","Trinidad and Tobago","Tuvalu","Taiwan","Tanzania","Ukraine","Uganda","Jarvis Island","Baker Island","Howland Island","Johnston Atoll","Midway Islands","Wake Island","United States","Uruguay","Uzbekistan","Vatican City","Saint Vincent and the Grenadines","Venezuela","British Virgin Islands","US Virgin Islands","Vietnam","Vanuatu","Wallis and Futuna","Samoa","Yemen","Mayotte","South Africa","Zambia","Zimbabwe"
             ]
         }
     }
@@ -108,11 +117,15 @@ class SearchEngine extends React.Component{
     constructor(props){
         super(props)
 
-        
+        this.state = {
+            currentSearchQuery: ""
+        }
     }
 
     getSearchQuery = (e) =>{
-        //console.log(e);
+        this.setState({
+            currentSearchQuery: e
+        })
     }
 
     getCountryInfo = (resp) =>{
@@ -122,11 +135,12 @@ class SearchEngine extends React.Component{
     }
 
     render() {
+        console.log(this.state.currentSearchQuery);
         return(
             <div className="searchEngine">
                 <SearchEngineBar getSearchQuery={this.getSearchQuery}/>
                 <div className="buttonWrapper">
-                    <SearchEngineButton searchQuery={this.props.clickedCountry} clickedCountry={this.props.clickedCountry} getCountryInfo={this.getCountryInfo} text="Search country"/>
+                    <SearchEngineButton currentSearchQuery={this.state.currentSearchQuery} searchQuery={this.props.clickedCountry} clickedCountry={this.props.clickedCountry} getCountryInfo={this.getCountryInfo} text="Search country"/>
                     <LuckySearchEngineButton getCountryInfo={this.getCountryInfo} text="Feeling lucky?"/>
                 </div>
             </div>
